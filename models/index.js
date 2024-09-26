4ad0503e5aba50f50be21f6381f34a167c6eb2d9
Jonathan Chen <jontkc98@u.washington.edu>
2024-09-25 19:02:11 -0700

    Create index.js
    Added table relations for models

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const sequelize = require('../config/database');
+const Game = require('game');
+const Team = require('team');
+const Player = require('player');
+const Champion = require('champion');
+
+// Define associations
+Game.belongsTo(Team, { foreignKey: 'team_1_id', as: 'Team1' });
+Game.belongsTo(Team, { foreignKey: 'team_2_id', as: 'Team2' });
+Team.hasMany(Game, { foreignKey: 'team_1_id' });
+Team.hasMany(Game, { foreignKey: 'team_2_id' });
+
+Player.belongsTo(Team, { foreignKey: 'team_id' });
+Team.hasMany(Player, { foreignKey: 'team_id' });
+
+Game.hasMany(Champion, { foreignKey: 'game_id' });
+Champion.belongsTo(Game, { foreignKey: 'game_id' });
+
+// Export models
+module.exports = {
+    sequelize,
+    Game,
+    Team,
+    Player,
+    Champion,
+};
